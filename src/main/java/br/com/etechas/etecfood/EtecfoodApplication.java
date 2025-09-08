a51af2759ade1c5fd15ec9ef4f75de20f8d0ee8d
Eliane <pf1551@fiap.com.br>
2025-09-08 13:30:52 -0300

    Projeto etec-food 2BI inicial

---
+++ src/main/java/br/com/etechas/etecfood/EtecfoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechas.etecfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtecfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtecfoodApplication.class, args);
+	}
+
+}
